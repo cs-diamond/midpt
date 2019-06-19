9f126fb7f9e45bb9592abd7361905b926bda3584
Bruce Wong <dbrucewong@gmail.com>
2019-06-19 10:52:20 -0700

    resolve yelpController diffs between Dean and Alex

--- server/yelpController.js
+++ server/yelpController.js
@@ -1,56 +1,91 @@
-
-'use strict';
-
 const yelp = require('yelp-fusion');
-
-// Place holder for Yelp Fusion's API Key. Grab them
-// from https://www.yelp.com/developers/v3/manage_app
 const apiKey = process.env.YELP_API_KEY;
 
-
-
 const yelpController = {
-  /*
   getRadius: (req, res, next) => {
+    let maxRad = 0;
+    function distance(lat1, lon1, lat2, lon2, unit) {
+      if (lat1 === lat2 && lon1 === lon2) {
+        return 0;
+      } else {
+        const radlat1 = (Math.PI * lat1) / 180;
+        const radlat2 = (Math.PI * lat2) / 180;
+        const theta = lon1 - lon2;
+        const radtheta = (Math.PI * theta) / 180;
+        let dist =
+          Math.sin(radlat1) * Math.sin(radlat2) +
+          Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        if (dist > 1) {
+          dist = 1;
+        }
+        dist = Math.acos(dist);
+        dist = (dist * 180) / Math.PI;
+        dist = dist * 60 * 1.1515;
+        if (unit === 'K') {
+          dist = dist * 1.609344;
+        }
+        if (unit === 'N') {
+          dist = dist * 0.8684;
+        }
+        return dist;
+      }
+    }
+
+    res.locals.isoIntersectionPoints.forEach(intersection => {
+      if (
+        distance(
+          res.locals.midpt.lat,
+          res.locals.midpt.lng,
+          intersection.lat,
+          intersection.lng,
+          'K'
+        ) > maxRad
+      ) {
+        maxRad = distance(
+          res.locals.midpt.lat,
+          res.locals.midpt.lng,
+          intersection.lat,
+          intersection.lng,
+          'K'
+        );
+      }
+    });
+
+    res.locals.radius = Math.floor(maxRad * 1000);
+    console.log(res.locals.radius);
     next();
   },
-  */
 
   getNearby: (req, res, next) => {
-    //console.log(res.locals);
     const searchRequest = {
-      //term:'Four Barrel Coffee',
       latitude: res.locals.midpt.lat,
       longitude: res.locals.midpt.lng,
-      categories: "coffee"
+      categories: 'coffee',
     };
-    
+
     const client = yelp.client(apiKey);
-    
-    client.search(searchRequest).then(response => {
-      const result = response.jsonBody.businesses;
-      let yelpData = [];
-      for (let i = 0; i < result.length;i++){
-        let resultAtI = result[i];
-        let lng = resultAtI.coordinates.longitude;
-        let lat = resultAtI.coordinates.latitude;
-        resultAtI.coordinates.lat = lat;
-        resultAtI.coordinates.lng = lng;
-        delete resultAtI.coordinates.longitude;
-        delete resultAtI.coordinates.latitude;
-        yelpData.push(resultAtI);
-        //latLong.push({lat:result[i].coordinates.latitude,lng:result[i].coordinates.longitude});
-      }
-      res.locals.yelpData = yelpData;
-      //console.log(res.locals);
-    
-      //const prettyJson = JSON.stringify(yelpData, null, 4);
-      //console.log(prettyJson);
-      return next();
-    })
-    .catch(e => {
-      console.log(e);
-    });
+
+    client
+      .search(searchRequest)
+      .then(response => {
+        const result = response.jsonBody.businesses;
+        let yelpData = [];
+        for (let i = 0; i < result.length; i++) {
+          let resultAtI = result[i];
+          let lng = resultAtI.coordinates.longitude;
+          let lat = resultAtI.coordinates.latitude;
+          resultAtI.coordinates.lat = lat;
+          resultAtI.coordinates.lng = lng;
+          delete resultAtI.coordinates.longitude;
+          delete resultAtI.coordinates.latitude;
+          yelpData.push(resultAtI);
+        }
+        res.locals.yelpData = yelpData;
+        return next();
+      })
+      .catch(e => {
+        console.log(e);
+      });
   },
 };
 
